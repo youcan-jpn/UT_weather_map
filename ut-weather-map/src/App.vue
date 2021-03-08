50bf486c29cee76f11c5e547f45ef690dd6b050c
yuta ono <youcana40627@gmail.com>
2021-03-08 19:13:34 +0900

    initial commit

---
+++ ut-weather-map/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>UT weather info</h1>
+      <WeatherInfo area="hongo" />
+      <WeatherInfo area="komaba" />
+      <WeatherInfo area="kashiwa" />
+  </div>
+</template>
+
+<script>
+import WeatherInfo from './components/WeatherInfo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WeatherInfo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ut-weather-map/src/components/WeatherInfo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>{{ eng2kanji(area) }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "WeatherInfo",
+    props: {
+        area: String
+    },
+    methods: {
+        eng2kanji: function(area) {
+            if (area === 'hongo') {
+                return "本郷"
+            } else if (area === 'komaba') {
+                return "駒場"
+            } else if (area === 'kashiwa') {
+                return "柏"
+            } else {
+                return "不明"
+            }
+        }
+    }
+}
+</script>
